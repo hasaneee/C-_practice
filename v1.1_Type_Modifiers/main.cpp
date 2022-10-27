fa1093c05a18a6e7398ff849f4431e23acd0ef9e
hasaneee <mahamudul.hasan349@gmail.com>
2022-10-27 16:25:55 +0600

    C++ practice Initialize

---
+++ v1.1_Type_Modifiers/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int i;
+    cout << "Size of int type: " << sizeof(i) <<endl;
+
+    float j;
+    cout << "Size of float type: " << sizeof(j) <<endl;
+
+    double k;
+    cout << "Size of double type: " << sizeof(k) <<endl;
+
+    char c;
+    cout << "Size of character type: " << sizeof(c) <<endl;
+
+    long int x;
+    cout << "Size of long int type: " << sizeof(x) <<endl;
+
+    short int y;
+    cout << "Size of short int type: " << sizeof(y) <<endl;
+
+    return 0;
+}
